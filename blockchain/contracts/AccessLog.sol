13f64f3ef8374e4811b8334da47af905a94d4900
saccharide <rtang@email.wm.edu>
2020-03-28 15:29:40 -0400

    update access log

--- blockchain/contracts/AccessLog.sol
+++ blockchain/contracts/AccessLog.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract AccessLog {
   address private owner;
-
+  mapping(address => [])
   event Log(
           address caller,
           uint file
@@ -17,8 +17,12 @@ contract AccessLog {
     _;
   }
 
+  function getFilename(String memory patientName, String memory birthday) public pure returns (uint256)  {
+      return keccak256(patientName, birthday);
+  }
 
   function log(uint file) public payable {
+      require()
       emit Log(msg.sender, file);
   }
 }
