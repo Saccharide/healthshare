2711160b56e33dab6f9f233c0adea92bab0f064a
saccharide <rtang@email.wm.edu>
2020-04-09 21:32:14 -0400

    fixed to string for address to string conversion

--- blockchain/contracts/AccessLog.sol
+++ blockchain/contracts/AccessLog.sol
@@ -247,11 +247,20 @@ contract AccessLog {
         return "Successfully added request to request board";
 
     }
-    function toString(address x) public pure returns (string memory) {
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
+
+    function toString(address _addr) public pure returns(string memory)
+    {
+        bytes32 value = bytes32(uint256(_addr));
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(51);
+        str[0] = '0';
+        str[1] = 'x';
+        for (uint256 i = 0; i < 20; i++) {
+            str[2+i*2] = alphabet[uint8(value[i + 12] >> 4)];
+            str[3+i*2] = alphabet[uint8(value[i + 12] & 0x0f)];
+        }
+        return string(str);
     }
 
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
